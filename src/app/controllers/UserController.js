9c2a7a20a4e0f058213644725cda680e5d50a833
Dailton Bastos <dailtonbastos@gmail.com>
2020-06-20 19:23:29 -0300

    Cadastro de usuário

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,23 @@
+const { User } = require('../models');
+
+class UserController {
+  create(req, res) {
+    return res.render('auth/signup');
+  }
+
+  async store(req, res) {
+    const { avatar, name, email, password, provider } = req.body;
+
+    await User.create({
+      avatar,
+      name,
+      email,
+      password,
+      provider,
+    });
+
+    return res.redirect('/');
+  }
+}
+
+module.exports = new UserController();
